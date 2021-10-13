211ac2b0e021566115c6c65d0586a672daf62647
jan <janek.grochulski@gmail.com>
2021-10-13 16:36:18 -0500

    Assignment Completed.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+function deleteByElementId(id) {
+    const target = document.querySelector(id);
+    target.remove();
+}
+
+deleteByElementId("main");
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+
+
+
